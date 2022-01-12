f436aae4a952518aa68da43dcb7f92a35b6128cb
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-01-11 22:14:56 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Bottles
+ * Pawelski
+ * 1/11/2022
+ * Read the code and run the program a few times to see what it does. 
+ * Once you are familiar with the program, answer the questions found 
+ * on the activity sheet.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 99; i >= 1; i--)
+            {
+                Console.WriteLine(i + " bottle(s) of pop on the wall.");
+                Console.WriteLine(i + " bottle(s) of pop.");
+                Console.WriteLine("Take one down, pass it around.");
+                Console.WriteLine((i - 1) + " bottle(s) of pop on the wall.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
